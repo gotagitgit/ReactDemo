9f98b871960bb542f060d267b1b2f7b053939406
Cyrus Penuliar <Cyrus.Penuliar@infor.com>
2025-07-02 07:25:02 +0800

    Counter using hooks implementation

--- src/counter/HookCounter.tsx
+++ src/counter/HookCounter.tsx
@@ -1,19 +1,34 @@
-import React from 'react';
+import React, { ChangeEvent, useEffect, useState } from 'react';
 
 function HookCounter() 
 {
+    const [count, setCount] = useState<number>(0);
+    const [name, setName] = useState<string>('');
+
+    const increment = () => setCount(count + 1);
+    const decrement = () => setCount(count - 1);
+    const reset = () => setCount(0);
+
+    useEffect(() => {
+        document.title = `Count: ${count}`;
+    }, [count]);
+
+    const handleNameChange = (e: ChangeEvent<HTMLInputElement>): void => setName(e.target.value);  
+
     return (
         <div className="counter">
-        <p>Hello 'Anonymous'!</p>
+        <p>Hello {name || 'Anonymous'}!</p>
         <input 
             type="text" 
             placeholder="Enter your name"
+            value={name}
+            onChange={handleNameChange}
         />
-        <h3>Count: </h3>
+        <h3>Count: {count}</h3>
         <div className="buttons">
-            <button>-</button>
-            <button>Reset</button>
-            <button>+</button>
+            <button onClick={decrement}>-</button>
+            <button onClick={reset}>Reset</button>
+            <button onClick={increment}>+</button>
         </div>
         </div>
     );
